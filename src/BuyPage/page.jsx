377cf56039ab006ac486219eb85d9632efb8af1c
Dancheez <foxden482@gmail.com>
2026-05-26 19:11:34 +0500

    jj

--- src/BuyPage/page.jsx
+++ src/BuyPage/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/Header'
+import Header from '../components/header'
 import MainForm from '../components/MainForm'
 import BuyTable from '../components/BuyTable'
 import Footer from '../components/Footer'
